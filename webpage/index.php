333c6c77e49df5ba7f12e2d8434b52b1409ae9b2
UmidAbdu <abdugaffarov23@gmail.com>
2021-04-04 22:48:34 +0500

    completed

--- webpage/index.php
+++ webpage/index.php
@@ -1,31 +1,155 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Validating Forms</title>
-		<link href="style.css" type="text/css" rel="stylesheet" />
-	</head>
-	
-	<body>
-		<h1>Registration Form</h1>
+<?php
+$name    = $email   = $uname = $pwd    = $c_pwd  = $date = "";
+$gender  = $marital = $addr  = $city   = $postal = $web  = "";
+$h_phone = $m_phone = $card  = $card_e = $salary = $gpa  = "";
 
-		<p>
-			This form validates user input and then displays "Thank You" page.
-		</p>
-		
-		<hr />
-		
-		<h2>Please, fill below fields correctly</h2>
-		<dl>
-			<dt>Name</dt>
-			<dd>
-				???
-			</dd>
-			
-			<!-- Write other fiels similar to Name as specified in lab6.pdf -->
-		</dl>
-		
-		<div>
-			Register
-		</div>
-	</body>
+$mapping = [
+    "name"    => [
+        "Name", "It has to contain at least 2 chars. It should not contain any number",
+        "([^0-9.@]){2,50}"
+    ],
+    "email"   => [
+        "Email", "It should correspond to valid email format",
+        "[\w\-+]+([.][\w]+)?@[\w\-+]+([.][a-z]{2,})+"
+    ],
+    "uname"   => [
+        "Username", "It has to contain at least 5 chars",
+        "[a-z][^ !@#$%^&*()=\[\]]{4,}"
+    ],
+    "pwd"     => [
+        "Password", "It has to contain at least 8 chars",
+        "[^\n]{8,}"
+    ],
+    "c_pwd"   => [
+        "Confirm Password", "It has to be equal to Password field",
+        "[^\n]{8,}"
+    ],
+    "date"    => [
+        "Date of Birth", "It should be written in dd.mm.yyyy format",
+        "[0-3][0-9]\.[0-1][0-2]\.[1-9][0-9]{3}"
+    ],
+    "gender"  => [
+        "Gender", "Only 2 options accepted: Male, Female",
+        "([Ff]e)?[Mm]ale"
+    ],
+    "marital" => [
+        "Marital Status", "Only 4 options accepted: Single, Married, Divorced, Widowed",
+        "([Ss]ingle|[Mm]arried|[Dd]ivorced|[Ww]idowed)"
+    ],
+    "addr"    => [
+        "Address", "It should contain at least 10 characters",
+        ".{10,}",
+    ],
+    "city"    => [
+        "City", "It should contain at least 2 characters",
+        ".{2,}",
+    ],
+    "postal"  => [
+        "Postal Code", "It should follow 6 digit format",
+        "(?(?=.*[1-9])[0-9]|[1-9])[0-9]{5}",
+    ],
+    "h_phone" => [
+        "Home Phone", "It should follow 9 digit format",
+        "[0-9]{9}",
+    ],
+    "m_phone" => [
+        "Mobile Phone", "It should follow 9 digit format",
+        "[0-9]{9}"
+    ],
+    "card"    => [
+        "Credit Card Number", "It should follow 16 digit format",
+        "[0-9]{16}"
+    ],
+    "card_e"  => [
+        "Credit Card Expiry Date", "It should be written in dd.mm.yyyy format",
+        "[0-3][0-9].[0-1][0-2].[1-9][0-9]{3}"
+    ],
+    "salary"  => [
+        "Monthly Salary", "It should be written in following format UZS 200,000.00",
+        "UZS [1-9][0-9]*(\.[0-9]{2})?"
+    ],
+    "web"     => [
+        "Web Site URL", "It should match URL format. For ex, http://github.com",
+        "(?:(?:https?|ftp):\/\/)?[\w\/\-?=%.]+\.[\w\/\-&?=%.]+"
+    ],
+    "gpa"     => [
+        "Overall GPA", "It should be a floating point number less than 4.5",
+        "(?(?=..[0-5])[0-4]|[0-3])\.[0-9]"
+    ]
+];
+
+$pwd_validation = "/\A(?=\w{6,10}\z)(?=[^a-z]*[a-z])(?=(?:[^A-Z]*[A-Z]){3})\D*\d.*\z/";
+
+$msg = $info = "";
+$color = "red";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $match = True;
+    foreach ($mapping as $key => $value) ${$key} = $_POST[$key];
+
+    foreach ($mapping as $key => $value){
+        if ($_POST[$key] == ""){
+            $msg = "Not all fields are filled!";
+            $match = False;
+            break;
+        }
+
+        if (!preg_match("/^$value[2]$/", $_POST[$key])){
+            $msg = "Field '$value[0]' filled incorrectly!";
+            $info = $value[1];
+            $match = False;
+            break;
+        }
+    }
+    if ($match){
+        if ($pwd != $c_pwd){
+            $msg = "Passwords don't match!";
+            $match = False;
+        } elseif (!preg_match($pwd_validation, $pwd)){
+            $msg = "Password validation failed!";
+            $info = "It must be 6 to 10 chars and must contain at least 1 lowercase, 3 uppercase, 1 digit characters";
+            $match = False;
+        }
+    }
+
+    if ($match){
+        $msg = "Registration Successful";
+        $info = "Thank you for choosing us!";
+        $color = "green";
+    }
+}
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+    <title>Validating Forms</title>
+    <link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+	
+<body>
+    <div class="form">
+        <h1>Registration Form</h1>
+
+        <p>This form validates user input and then displays "Thank You" page.</p>
+        <hr/>
+
+        <h2>Please, fill below fields correctly</h2>
+        <form action="index.php" method="post">
+        <dl>
+            <?php
+            foreach($mapping as $var => $title){
+                echo "<dt>$title[0]</dt>\n";
+                echo "<dd><label><input name=$var value='${$var}'></label></dd>\n";
+            }
+            ?>
+        </dl>
+        <?= "<div class='status' style='color: $color'>$msg<br/>$info</div>" ?>
+        <div>
+            <input type="submit" value="Register">
+        </div>
+        </form>
+    </div>
+</body>
 </html>
--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,57 +1,127 @@
 <?php
 
-	$pattern="";
-	$text="";
-	$replaceText="";
-	$replacedText="";
+$pattern = "";
+$text    = "";
+$r_text  = "";
+$rd_text = "";
 
-	$match="Not checked yet.";
+$method  = "";
+$string  = "";
+$output  = "";
 
-if ($_SERVER["REQUEST_METHOD"]=="POST") {
-	$pattern=$_POST["pattern"];
-	$text=$_POST["text"];
-	$replaceText=$_POST["replaceText"];
+$match   = "Not checked yet.";
 
-	$replacedText=preg_replace($pattern, $replaceText, $text);
+function sel($a): string {
+    global $method;
+    return $method == $a? "selected": "";
+}
 
-	if(preg_match($pattern, $text)) {
-						$match="Match!";
-					} else {
-						$match="Does not match!";
-					}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["check_btn"])){
+        $pattern = $_POST["pattern"];
+        $text    = $_POST["text"   ];
+        $r_text  = $_POST["r_text" ];
+
+        $rd_text = preg_replace("/$pattern/", $r_text, $text);
+        $match   = preg_match  ("/$pattern/", $text)? "Match!": "Does not match!";
+
+        // Version 1:
+        if (preg_match("/.*quick.*/", $text))
+            $match = "Match: quick";
+        elseif (preg_match("/^[\w\d]+@[\w\d]+[\w\d.]+$/", $text))
+            $match = "Match: email";
+        elseif (preg_match("/^\+998-?[\d]{2}-?[\d]{3}-?[\d]{2}-?[\d]{2}$/", $text))
+            $match = "Match: phone number";
+
+//        // Version 2:
+//        $matches = [
+//            "/.*quick.*/" => "quick",
+//            "/[\w\d]+@[\w\d]+.[\w\d]+/" => "email",
+//            "/^\+998-?[\d]{2}-?[\d]{3}-?[\d]{2}-?[\d]{2}$/" => "phone number"
+//        ];
+//        foreach ($matches as $key => $value) {
+//            if (preg_match($key, $text)) { $match = "Match: ".$value; break;}
+//        }
+
+    } elseif (isset($_POST["run_btn"])){
+        $mapping = [
+            "ws" => "/\s+/"   , // whitespace
+            "nn" => "/[^0-9]/", // nonnumerical
+            "nl" => "/\R/"    , // new line
+        ];
+        $method = $_POST["method"];
+        $string = $_POST["string"];
+
+        if (key_exists($method, $mapping)){
+            $output = preg_replace($mapping[$method], "", $string);
+        }
+        elseif ($method == "et"){ // extract text
+            $matches = array();
+            preg_match_all("/[\[(](.*)[)\]]/", $string, $matches);
+            $output = implode(", ", $matches[1]);
+        }
+    }
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Valid Form</title>
+    <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-	<form action="regex_valid_form.php" method="post">
-		<dl>
-			<dt>Pattern</dt>
-			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
+    <div class="form">
+        <form action="regex_valid_form.php" method="post">
+            <dl>
+                <dt>Pattern      </dt>
+                <dd><label><input type="text" name="pattern" value="<?=$pattern?>"></label></dd>
 
-			<dt>Text</dt>
-			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
+                <dt>Text         </dt>
+                <dd><label><input type="text" name="text"    value="<?=$text?>">   </label></dd>
 
-			<dt>Replace Text</dt>
-			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
+                <dt>Replace Text </dt>
+                <dd><label><input type="text" name="r_text"  value="<?=$r_text?>"> </label></dd>
 
-			<dt>Output Text</dt>
-			<dd><?=	$match ?></dd>
+                <dt>Output Text  </dt>
+                <dd><?=$match?></dd>
 
-			<dt>Replaced Text</dt>
-			<dd> <code><?=	$replacedText ?></code></dd>
+                <dt>Replaced Text</dt>
+                <dd><code><?=$rd_text?></code></dd>
 
-			<dt>&nbsp;</dt>
-			<dd><input type="submit" value="Check"></dd>
-		</dl>
+                <dt>&nbsp;</dt>
+                <input type="submit" name="check_btn" value="Check">
+            </dl>
+        </form>
+    </div>
+    <div class="form">
+        <form action="regex_valid_form.php" method="post" id="additional">
+            <div class="block">
+                <p>String:</p>
+                <div id="output">
+                    <label for="txtArea"></label>
+                    <textarea form="additional" name="string" id="txtArea" wrap="soft"><?=$string?></textarea>
+                </div>
+            </div>
 
-	</form>
+            <div class="block">
+                <p>Output:</p>
+                <div id="output"><?=$output?></div>
+            </div>
+
+            <div class="block">
+                <label for="method">Choose method:</label>
+                <select id="method" name="method">
+                    <option <?=sel("ws")?> value="ws"> Remove whitespaces             </option>
+                    <option <?=sel("nn")?> value="nn"> Remove nonnumerical characters </option>
+                    <option <?=sel("nl")?> value="nl"> Remove new lines               </option>
+                    <option <?=sel("et")?> value="et"> Extract text                   </option>
+                </select>
+            </div>
+            <input type="submit" name="run_btn" value="Run">
+        </form>
+    </div>
 </body>
 </html>
